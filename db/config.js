d9f94b7995bfbb859defcb67f001fe8d5b3e79a4
kyle004 <kyleberman92@gmail.com>
2021-07-22 20:48:20 -0700

    added screenshot of app

--- db/config.js
+++ db/config.js
@@ -1,12 +1,3 @@
 const { Sequelize } = require('sequelize')
 
-let connection = new Sequelize(process.env.LOCAL_DB, process.env.LOCAL_UN, process.env.LOCAL_PW, {
-  host: 'localhost',
-  dialect: 'mysql'
-})
-
-if (process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL)
-}
-
-module.exports = connection
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_URL)
